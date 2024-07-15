663eb7ae06a41260252a5f27a6c9ca459cb0a1a2
chanhwiim <chanhwiim459@gmail.com>
2024-07-15 13:28:51 +0900

    feat: 동물 소리 추가

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 public class Main {
+
     public static void main(String[] args) {
         Zoo zoo = new Zoo();
+        AnimalSound animalSound = new AnimalSound();
+
         System.out.println(zoo.lionSound());
+        System.out.printf(animalSound.getTigerSound());
     }
 }
