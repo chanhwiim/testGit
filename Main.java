0aec0c4379e13762ce222a719af403ead700e30a
AoiTuNa <bestspark123@naver.com>
2024-07-15 13:22:32 +0900

    feat: add lion sound

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+        System.out.println(zoo.lionSound());
+    }
+}
---
+++ Zoo.java
@@ -0,0 +1,9 @@
+public class Zoo {
+    private static String LION = "lion";
+    private static String Tiger = "tiger";
+    private static String Panzer = "88mm";
+
+    public String lionSound(){
+        return "grrrrr....";
+    }
+}
